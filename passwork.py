a3b4c22d4a97691fcf5f34b049ff5b8a0d58877b
PoHsuanChou <paul4017boy@gmail.com>
2020-01-16 16:25:47 +0800

    new version

--- passwork.py
+++ passwork.py
@@ -1,6 +1,6 @@
 password = 'a123456'
 i= 3
-while True:
+while i >0:
 	ans = input('請輸入密碼？')
 
 	if ans == password:
@@ -9,5 +9,4 @@ while True:
 	else:
 		i = i - 1
 		print('密碼錯誤！剩下 ', i ,'機會')
-		if i ==0:
-			break
+	
