6e7941692c26fb0e85f821756dc55fd470c51ff4
PoHsuanChou <paul4017boy@gmail.com>
2020-01-17 16:48:26 +0800

    second version

--- r.py
+++ r.py
@@ -1,6 +1,8 @@
 import random
 r = random.randint(1, 100)
+count = 0
 while True:
+	count += 1 
 	number = input('請輸入數字？')
 	number = int(number)
 	if number == r:
@@ -12,3 +14,4 @@ while True:
 	elif number<r:
 		
 		print('比答案小')
+	print('這是你猜的第',count ,'次')
